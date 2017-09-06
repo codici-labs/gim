76a66725adc9bbda35cd4b7711ff2fb3f52c4235
Diseño8a <diseno8a@iMac-de-Diseno8a.local>
2017-09-06 18:15:32 -0300

    base con roles actualizada

--- gimnasio.sql
+++ gimnasio.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 06-09-2017 a las 16:04:52
+-- Tiempo de generación: 06-09-2017 a las 18:12:48
 -- Versión del servidor: 5.5.54-0+deb8u1
 -- Versión de PHP: 5.6.30-0+deb8u1
 
@@ -84,15 +84,16 @@ CREATE TABLE IF NOT EXISTS `roles` (
 `id` int(11) NOT NULL,
   `role` varchar(255) NOT NULL,
   `default` tinyint(2) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `roles`
 --
 
 INSERT INTO `roles` (`id`, `role`, `default`) VALUES
-(1, 'admin', 0),
-(2, 'user', 1);
+(1, 'sysadmin', 1),
+(2, 'admin', 2),
+(3, 'user', 3);
 
 -- --------------------------------------------------------
 
@@ -145,15 +146,17 @@ CREATE TABLE IF NOT EXISTS `users` (
   `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `role_id` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Volcado de datos para la tabla `users`
 --
 
 INSERT INTO `users` (`id`, `firstname`, `lastname`, `puesto`, `sede_id`, `telefono`, `interno`, `celular`, `username`, `password`, `email`, `activated`, `banned`, `ban_reason`, `new_password_key`, `new_password_requested`, `new_email`, `new_email_key`, `last_ip`, `last_login`, `created`, `modified`, `role_id`) VALUES
-(10, 'Martin', 'Pandolfelli', '2', 1, 44444, 123, 15, 'admin', '$2a$08$k9MpHg7SSJ.z2AEpXJM6f.KVPapaDX/Di7D/.Aqy5bm.Sb6loQhpy', 'martinpandolfelli@gmail.com', 1, 0, NULL, NULL, NULL, NULL, NULL, '200.115.250.117', '2017-09-05 14:36:40', '2017-08-22 20:36:27', '2017-09-06 17:40:16', 1),
-(11, 'Admin', 'Admin', '2', 1, 444, 44, 44, '', '$2a$08$dbJMAlWArnzNdkZHLQOG4eZilDwu9CSIvbxSrhjcmroE7wGm9uwXm', 'admin@codicilabs.com', 1, 0, NULL, NULL, NULL, NULL, NULL, '190.210.134.121', '2017-09-06 15:48:46', '2017-09-04 18:41:04', '2017-09-06 18:48:46', 2);
+(10, 'Martin', 'Pandolfelli', '2', 1, 44444, 123, 15, 'admin', '$2a$08$k9MpHg7SSJ.z2AEpXJM6f.KVPapaDX/Di7D/.Aqy5bm.Sb6loQhpy', 'martinpandolfelli@gmail.com', 1, 0, NULL, NULL, NULL, NULL, NULL, '200.115.250.117', '2017-09-05 14:36:40', '2017-08-22 20:36:27', '2017-09-06 20:21:43', 2),
+(11, 'Admin', 'Admin', '2', 1, 444, 44, 44, '', '$2a$08$dbJMAlWArnzNdkZHLQOG4eZilDwu9CSIvbxSrhjcmroE7wGm9uwXm', 'admin@codicilabs.com', 1, 0, NULL, NULL, NULL, NULL, NULL, '190.210.134.121', '2017-09-06 18:02:56', '2017-09-04 18:41:04', '2017-09-06 21:02:56', 2),
+(12, 'Sysadmin', 'Sysadmin', '2', 1, 12342145, 123, 77322233, '', '$2a$08$imaLjp6wD.kQEgKcQrtHrOI7qjkC57e5D00SK3R4GxU7MbfPawp2q', 'sysadmin@codicilabs.com', 1, 0, NULL, NULL, NULL, NULL, NULL, '190.210.134.121', '2017-09-06 17:22:33', '2017-09-06 16:41:24', '2017-09-06 20:23:42', 1),
+(13, 'user', 'user', '2', 1, 222, 22, 222, '', '$2a$08$MxeKpRjTKaUta5lT3M3XNeb1hl3RKqfp7pFz1zrQ7zKpUsx/8rVuu', 'user@codicilabs.com', 1, 0, NULL, NULL, NULL, NULL, NULL, '190.210.134.121', '2017-09-06 18:09:49', '2017-09-06 18:01:30', '2017-09-06 21:09:49', 3);
 
 -- --------------------------------------------------------
 
@@ -182,7 +185,7 @@ CREATE TABLE IF NOT EXISTS `user_profiles` (
   `website` varchar(255) COLLATE utf8_bin DEFAULT NULL,
   `facebook_id` varchar(255) COLLATE utf8_bin DEFAULT NULL,
   `facebook_token` varchar(255) COLLATE utf8_bin DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Volcado de datos para la tabla `user_profiles`
@@ -195,7 +198,9 @@ INSERT INTO `user_profiles` (`id`, `user_id`, `country`, `website`, `facebook_id
 (4, 8, NULL, NULL, NULL, NULL),
 (5, 9, NULL, NULL, NULL, NULL),
 (6, 10, NULL, NULL, NULL, NULL),
-(7, 11, NULL, NULL, NULL, NULL);
+(7, 11, NULL, NULL, NULL, NULL),
+(8, 12, NULL, NULL, NULL, NULL),
+(9, 13, NULL, NULL, NULL, NULL);
 
 --
 -- Índices para tablas volcadas
@@ -267,7 +272,7 @@ MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 -- AUTO_INCREMENT de la tabla `roles`
 --
 ALTER TABLE `roles`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT de la tabla `sedes`
 --
@@ -277,12 +282,12 @@ MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=12;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=14;
 --
 -- AUTO_INCREMENT de la tabla `user_profiles`
 --
 ALTER TABLE `user_profiles`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
